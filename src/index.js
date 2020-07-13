66942d9d39223e84899de1deffd29f11da138eef
Stepan Zverev <stepanzverev@gmail.com>
2020-07-13 14:38:37 +0300

    Подключены скрипты

--- src/index.js
+++ src/index.js
@@ -3,6 +3,55 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase'
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyD6DnGbVfdJlDJ_pEOUfDfTDJrA8j3lIs8",
+  authDomain: "dv-inventory.firebaseapp.com",
+  databaseURL: "https://dv-inventory.firebaseio.com",
+  projectId: "dv-inventory",
+  storageBucket: "dv-inventory.appspot.com",
+  messagingSenderId: "130062240176",
+  appId: "1:130062240176:web:ecbca5d29b37d25c6cee75"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);	
+
+
+firebase.firestore().collection("places").get().then(response => { 
+  let docs = response.docs.map(x => {
+    try {
+    return { 
+         id: x.id, 
+         data: x.data(), 
+         parts:  x.data().parts && x.data().parts.map(part => part.id) 
+  }
+} catch {
+
+}
+return null
+
+}); 
+  console.info("places", docs); 
+});
+
+firebase.firestore().collection("inventory").get().then(response => { 
+  let docs = response.docs.map(x => {
+    try {
+    return { 
+         id: x.id, 
+         data: x.data(), 
+         placeId: x.data().place.id 
+  }
+    } catch {}
+return null
+
+}); 
+  console.info("inventory",docs); 
+});
+
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +60,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
 serviceWorker.unregister();
