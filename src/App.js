4e6969b2d8c467d2b6d567c608554a7c3ced68a8
Stepan Zverev <stepanzverev@gmail.com>
2020-07-13 22:38:05 +0300

    запросы к серверу перенесены в компонент App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,56 @@ import React from 'react';
 import classes from './App.css';
 import Hierarchy from "./containers/Hierarchy/Hierarchy"
 import Store from "./containers/Store/Store"
-
+import firebase from "firebase"
 
 class App extends React.Component {
 
   state = {
-    currentPlace: null
+    currentPlace: null,
+    rootItemsId: ['main', 'production'],
+    places: [],
+    inventory: []
+  }
+
+  componentWillMount() {
+
+    firebase.firestore().collection("places").get().then(response => {
+      let places = response.docs.map(x => {
+        try {
+          return {
+            id: x.id,
+            data: x.data(),
+            parts: x.data().parts && x.data().parts.map(part => part.id)
+          }
+        } catch {
+
+        }
+        return null
+
+      });
+      console.info("places", places);
+      this.setState({
+        places
+      })
+    });
+
+    firebase.firestore().collection("inventory").get().then(response => {
+      let inventory = response.docs.map(x => {
+        try {
+          return {
+            id: x.id,
+            data: x.data(),
+            placeId: x.data().place.id
+          }
+        } catch { }
+        return null
+
+      });
+      console.info("inventory", inventory);
+      this.setState({
+        inventory
+      })
+    });
   }
 
   onPlaceClickHandler = item => {
@@ -20,8 +64,12 @@ class App extends React.Component {
   render() {
     return (
       <div className={classes.App}>
-        <Hierarchy onPlaceClick={this.onPlaceClickHandler}/>
-        <Store currentRoom={this.state.currentPlace}/>
+        <Hierarchy
+          places={this.state.places}
+          rootItemsId={this.state.rootItemsId}
+          onPlaceClick={this.onPlaceClickHandler}
+        />
+        <Store currentRoom={this.state.currentPlace} />
       </div>
     )
   }
--- src/containers/Hierarchy/Hierarchy.js
+++ src/containers/Hierarchy/Hierarchy.js
@@ -1,62 +1,14 @@
 import React, { Component } from 'react'
 import classes from "./Hierarchy.css"
-import firebase from "firebase"
+
 import Place from "../../components/Place/Place"
 
 class Hierarchy extends Component {
 
-    state = {
-        rootItemsId: ['main', 'production'],
-        places: [],
-        inventory: []
-    }
-
-    componentWillMount() {
-
-        firebase.firestore().collection("places").get().then(response => {
-            let places = response.docs.map(x => {
-                try {
-                    return {
-                        id: x.id,
-                        data: x.data(),
-                        parts: x.data().parts && x.data().parts.map(part => part.id)
-                    }
-                } catch {
-
-                }
-                return null
-
-            });
-            console.info("places", places);
-            this.setState({
-                places
-            })
-        });
-
-        firebase.firestore().collection("inventory").get().then(response => {
-            let inventory = response.docs.map(x => {
-                try {
-                    return {
-                        id: x.id,
-                        data: x.data(),
-                        placeId: x.data().place.id
-                    }
-                } catch { }
-                return null
-
-            });
-            console.info("inventory", inventory);
-            this.setState({
-                inventory
-            })
-        });
-
-    }
-
     renderRootItems = places => {
 
         return places.map((item, index) => {
-            if (this.state.rootItemsId.indexOf(item.id) !== -1) {
+            if (this.props.rootItemsId.indexOf(item.id) !== -1) {
 
             return (<ul><Place onPlaceClick={this.props.onPlaceClick} key={index} place={item} placeList={places}/></ul>)
             } else {
@@ -68,7 +20,7 @@ class Hierarchy extends Component {
     render() {
         return (
             <div className={classes.Hierarchy}>
-                {this.state.places ? this.renderRootItems(this.state.places) : null}
+                {this.props.places ? this.renderRootItems(this.props.places) : null}
             </div>
         )
     }
