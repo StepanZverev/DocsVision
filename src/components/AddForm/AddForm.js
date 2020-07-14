5157efbebdaed11c969e51fdf61b76d9c330d165
Stepan Zverev <stepanzverev@gmail.com>
2020-07-15 01:53:24 +0300

    Форма отчищается

--- src/components/AddForm/AddForm.js
+++ src/components/AddForm/AddForm.js
@@ -43,24 +43,47 @@ class AddForm extends Component {
         }
     }
 
+    clickHandler = event => {
+        this.props.onAddItem(event, this.state.name, this.state.count)
+
+        this.setState({
+            name: null,
+            count: null,
+            isValidName: false,
+            isValidCount: true,
+            toched: false
+        })
+    }
+
     render() {
         return (
             <form>
                 <h3>Добавить оборудование:</h3>
                 <label>
                     Название
-                <input type="text" onChange={event => this.onChangeHandler(event, true)}/>
+                <input 
+                    type="text" 
+                    value={this.state.name ? this.state.name : ""} 
+                    onChange={event => this.onChangeHandler(event, true)} 
+                />
 
                 </label>
                 <label>
                     Колличество
-                <input type="number" placeholder={1} onChange={event => this.onChangeHandler(event, false)}/>
+
+                <input 
+                    type="number" 
+                    value={this.state.count ? this.state.count : ""} 
+                    placeholder={1} 
+                    onChange={event => this.onChangeHandler(event, false)} 
+                />
                 </label>
-                <button 
-                onClick={event => this.props.onAddItem(event, this.state.name, this.state.count)}
-                disabled={!isValid(this.state.toched, this.state.isValidName, this.state.isValidCount)}
+
+                <button
+                    onClick={event => this.clickHandler(event)}
+                    disabled={!isValid(this.state.toched, this.state.isValidName, this.state.isValidCount)}
                 >Add</button>
-                
+
             </form>
         )
     }
